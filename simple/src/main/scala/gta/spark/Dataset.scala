2630fb3d6d3eef8cdfb574ec595ee3580d96be47
VenancioIgrejas <venancio.igrejas@poli.ufrj.br>
2018-03-27 16:34:13 -0300

    maven problem

--- simple/src/main/scala/gta/spark/Dataset.scala
+++ simple/src/main/scala/gta/spark/Dataset.scala
@@ -8,36 +8,43 @@ package gta.spark
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.DataFrame
+//import org.apache.spark.sql.SparkSession
+//import org.apache.spark.sql.DataFrame
 import org.apache.spark.rdd.RDD
-import Array._
+//import scala.collection.mutable.ArrayBuffer
 
 
 object  Dataset {
   val numberFeacture = 46
-  val master = "local[4]"//"spark://master:7077"
+  val master = "spark://master:7077"
 
   val conf: SparkConf = new SparkConf().setMaster(master).setAppName("CATRACA")
   val sc: SparkContext = new SparkContext(conf)
-
-  def inputMatriz(rddMatriz: Array[String]) = {
-    val array: Array[String] = Array()
-    for (x <- 3 to (rddMatriz.lenght -1) ){
-     array ++ Array(rddMatriz(x))
+/*
+  def inputMatriz(rddMatriz: Any) = {
+    for (i <- 0 to 3){
+      rddMatriz.remove(i)
     }
-    (Array(rddMatriz(0),rddMatriz(1),rddMatriz(2),rddMatriz(3)),array)
+    rddMatriz
   }
-
+*/
   def dataPreparing(lines: RDD[String]) = {
-    val virgulas = lines.map(x => x.split(',')).map(x => inputMatriz(x)))
+    val virgulas = lines.map(_.split(',').toBuffer).map(x => {
+      for(i <- 0 to 3){
+        x.remove(0)
+      }
+      x
+    })
+    val classes = virgulas.map(x => x(x.length -1))
+    val test = virgulas.map(x => x - x(x.length -1))
+    (test,classes)
   }
 
   def main(args: Array[String]){
     val inputPath = args(0)
-    val (vactor,classes) = dataPreparing(sc.textFile(inputPath))
-
-    println(inputPath)
+    val (vector,classes) = dataPreparing(sc.textFile(inputPath))
+    println(vector.take(1))
+    println(classes.take(1))
     sc.stop()
   }
 }
